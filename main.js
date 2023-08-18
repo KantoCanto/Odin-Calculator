8877d66705e8a7707e497e8ad3253b8967ab7cc3
KantoCanto <bruno.canto.neves@gmail.com>
2023-08-18 14:53:18 +0100

    almost functionall, need to fix .slice is not a function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let operator = '';
-let previousValue = '';
-let currentValue = '';
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -20,14 +20,82 @@ document.addEventListener("DOMContentLoaded", function(){
     //add an event listener to all the number buttons
     numbers.forEach((number) => number.addEventListener("click", function(e){
         handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
     }))
+
+    operators.forEach((op) => op.addEventListener("click", function(e){
+        handleOperator(e.target.textContent);
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = "";
+        currentValue = "";
+        operator = "";
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
+
+    equal.addEventListener("click", function(){
+        if(currentValue != "" && previousValue != ""){
+            operate();
+            previousScreen.textContent = "";
+            if (previousValue.length <= 5) {
+              currentScreen.textContent = previousValue;
+            } else {
+              currentScreen.textContent = previousValue.slice(0, 5) + "...";
+            }
+        }
+    })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+    })
 })
 
 
 function handleNumber(num) {
-    console.log(num);
-    // if(currentValue.length <= 5){
-    //     currentValue += num;
-    // }
+    if(currentValue.length <= 5){
+     currentValue += num;
+    }
 }
 
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = "";
+}
+
+function operate(){
+
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    switch(operator){
+        case "+" :
+            return previousValue += currentValue;
+        case "-" : 
+            return previousValue -= currentValue;
+        case "/" :
+            return previousValue /= currentValue;
+        case "*" :
+            return previousValue *= currentValue;
+        default:
+           break;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += ".";
+    }
+}
